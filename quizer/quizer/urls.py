31feffbc2d57830eedfe9a7598575a41a66f5f12
LeadNess <leadness@mail.ru>
2020-09-07 22:36:49 +0300

    Fix static files collecting
    Update collectstatic settings
    Move static to main app folder
    Update microservice_config

--- quizer/quizer/urls.py
+++ quizer/quizer/urls.py
@@ -14,13 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', include('main.urls')),
 ]
-
-# if settings.DEBUG:
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()

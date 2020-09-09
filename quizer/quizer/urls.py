51f9543b372da86aa81a5f7d4e73eccd82620a6d
LeadNess <leadness@mail.ru>
2020-09-09 21:25:08 +0300

    Fix media files displaying
    Fix quizer/quizer/urls.py for providing media files displaying in production

--- quizer/quizer/urls.py
+++ quizer/quizer/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.urls import path, include
+from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', include('main.urls')),
-]
-urlpatterns += staticfiles_urlpatterns()
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
